b15e37fcedca0d96f3cbc640b4d4fd514d6f29e2
amit-kumar50 <amit.kumar50@sap.com>
2023-02-28 18:28:05 +0530

    pushing object having syntax Error

---
+++ src/main/java/com/hackathon03/codeguru/exception/SyntaxError.java
@@ -0,0 +1,13 @@
+package com.hackathon03.codeguru.exception;
+
+public class SyntaxError {
+
+    public void callObjectHavingSyntaxError() {
+    Integer a =23;
+    String x="Test"
+    System.out.println("Inside class SyntaxError");
+
+    }
+
+
+}
