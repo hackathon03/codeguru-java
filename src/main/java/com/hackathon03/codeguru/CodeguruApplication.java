8f373cf3339ce05fe815f0eca6d5b092d4ecde8b
amit-kumar50 <amit.kumar50@sap.com>
2023-02-28 15:56:44 +0530

    adding initial file for java project

---
+++ src/main/java/com/hackathon03/codeguru/CodeguruApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon03.codeguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeguruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon03/codeguru/CodeguruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon03.codeguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
